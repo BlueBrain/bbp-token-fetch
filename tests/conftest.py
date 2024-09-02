2ea837f27eaa9b1edf9d206cab1393b012fb2afa
mouffok <sarah.mouffok@epfl.ch>
2024-09-02 12:06:04 +0200

    interrupt all refresh jobs when tests end

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+from blue_brain_token_fetch.job import InterruptionStack
 
 SERVICE_CONFIG = "./tests/tests_data/service_keycloak_config.yaml"
 REGULAR_CONFIG_WITH_CLIENT_PWD = "./tests/tests_data/regular_keycloak_config_with_password.yaml"
@@ -31,3 +32,7 @@ def service_username(pytestconfig):
 @pytest.fixture(scope="session")
 def service_password(pytestconfig):
     return pytestconfig.getoption("service_password")
+
+
+def pytest_sessionfinish(session, exitstatus):
+    InterruptionStack.callable_stack()
